25eafd8d13bee953ee92401c65778dccebac3053
MUHAMMAD BILAL SIDDIQUI <63120839+bilo1188@users.noreply.github.com>
2020-10-12 20:37:46 +0500

    Add files via upload

---
+++ 5 Static Methods In Python oops  Object Oriented Programming.py
@@ -0,0 +1,38 @@
+class Employee:
+
+    increment =  2
+
+    def __init__ (self,fname,lname,salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+
+    def increase(self):
+        self.salary = int(self.salary * self.increment)
+
+    @classmethod
+
+    def change_increment(cls,amount):
+        
+        cls.increment = amount
+    @classmethod
+
+    def from_str(cls,emp_string):
+        fname,lname,salary = emp_string.split("-")
+        return cls(fname,lname,salary)
+    @staticmethod
+
+    def isopen(day):
+        if day=='sunday':
+            return False
+        else:
+            return True
+
+#print(bilal.salary )       
+#Employee.change_increment(5)
+#bilal.increase()
+#print(bilal.salary )        
+#xyz = Employee.from_str("quaid-azam-1000")
+#print(xyz.salary)
+bilal = Employee('bilal','prof',10000)        
+print(Employee.isopen('sunday'))
